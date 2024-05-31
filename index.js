3de39b6c01427b6c6469a24189ec2fdaee1cf4c2
Oleg Plevan <oleg.plevan@gmail.com>
2024-05-31 12:04:01 +0100

    add /health endpoint

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
+app.get("/health", (req, res) => res.send({ message: "Life is good." }));
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
